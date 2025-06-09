ef92144509e82842ce536caab239231efada85aa
madhusudankokari <madhusudankokari08@gmail.com>
2025-06-09 19:05:28 +0530

    phase1 changes

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const Listing = require("C:/Users/madhu/Downloads/majorproject/models/listing.js
 const path=require("path");
 const methodOverride = require("method-override");
 const ejsMate=require("ejs-mate");
-
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js")
+const {listingSchema}=require("./schema.js");
 const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
 
 main()
@@ -20,6 +22,8 @@ async function main(){
     await mongoose.connect(MONGO_URL);
 }
 
+
+
 app.set("view engine","ejs")
 app.set("views",path.join(__dirname,"views"));
 
@@ -30,67 +34,86 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.get("/",(req,res) => {
     res.send("hi,i am root");
 });
+const validateListing=(req,res,next)=>{
+    let{error}=listingSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+      throw new ExpressError(400,errMsg);
+    }else{
+       next(); 
+    }
+};
 
 //index route
-app.get("/listings",async(req,res)=>{
+app.get("/listings",wrapAsync(async(req,res)=>{
    const allListings=await Listing.find({});
-   res.render("listings/index",{allListings});
+   res.render("listings/index.ejs",{allListings});
     
 
-});
+}));
 
 //new route
-app.get("/Listings/new",(req,res)=>{
+app.get("/listings/new",(req,res)=>{
       res.render("listings/new.ejs");
 });
 
 
 // show route
 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id",wrapAsync( async (req, res) => {
     let {id} = req.params;
-    try {
+   
         const listing = await Listing.findById(id);
         
         if (!listing) {
             return res.status(404).send("Listing not found");
         }
         res.render("listings/show.ejs", { listing });
-    } catch (err) {
-        // ID was probably malformed
-        res.status(400).send("Invalid ID format");
-    }
-});
+    } 
+));
 
 //create route
-app.post("/listings", async (req,res) =>{
+app.post("/listings", wrapAsync(async (req,res,next) =>{
+   
+   let result=listingSchema.validate(req.body);
+   console.log(result);
+   if(result.error){
+    throw new ExpressError(400,error);
+   }
     const newListing = new Listing(req.body.listing);
+    
     await newListing.save();
     res.redirect("/listings");
-})
+    }
+   
+));
 
 //edit route
-app.get("/listings/:id/edit", async (req,res) =>{
+app.get("/listings/:id/edit",wrapAsync( async (req,res) =>{
      let {id} = req.params;
      const listing = await Listing.findById(id);
      res.render("listings/edit.ejs",{listing});
-});
+}));
 
 //update route
-app.put("/listings/:id", async (req,res) => {
+app.put("/listings/:id",wrapAsync( async (req,res) => {
+     if(!req.body.listing){
+     throw new ExpressError(400,"Send valid data for listing")
+   }
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id,{ ...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+    
+}));
 
 
 //delete route
-app.delete("/listings/:id", async (req,res) =>{
+app.delete("/listings/:id",wrapAsync( async (req,res) =>{
     let {id} = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-});
+}));
 
 // app.get("/testListisng",async (req,res) =>{
 //     let sampleListing = new Listing({
@@ -105,6 +128,17 @@ app.delete("/listings/:id", async (req,res) =>{
 //     console.log("sample was saved");
 //     res.send("sucessfull testing");
 // }); 
+
+app.all(/.*/, (req, res, next) => {
+    next(new ExpressError(404, "Page not Found!"));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500 } = err;
+    if (!err.message) err.message = "Something went wrong!";
+    res.status(statusCode).render("error.ejs", { err });
+});
+
 
 app.listen(8080,() =>{
     console.log("server is listening to port 8080");
--- init/data.js
+++ init/data.js
@@ -1,16 +1,5 @@
 const sampleListings = [
-  {
-    title: "Cozy Beachfront Cottage",
-    description:
-      "Escape to this charming beachfront cottage for a relaxing getaway. Enjoy stunning ocean views and easy access to the beach.",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-    },
-    price: 1500,
-    location: "Malibu",
-    country: "United States",
-  },
+  
   {
     title: "Modern Loft in Downtown",
     description:
@@ -95,18 +84,7 @@ const sampleListings = [
     location: "Los Angeles",
     country: "United States",
   },
-  {
-    title: "Ski-In/Ski-Out Chalet",
-    description:
-      "Hit the slopes right from your doorstep in this ski-in/ski-out chalet in the Swiss Alps.",
-    image: {
-      filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1502784444187-359ac186c5bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNreSUyMHZhY2F0aW9ufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
-    },
-    price: 3000,
-    location: "Verbier",
-    country: "Switzerland",
-  },
+  
   {
     title: "Safari Lodge in the Serengeti",
     description:
